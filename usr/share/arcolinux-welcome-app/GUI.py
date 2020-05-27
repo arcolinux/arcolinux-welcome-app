22d39b97c3710001c5128df5b4f621df4e22ac77
Erik Dubois <erik.dubois@gmail.com>
2020-05-27 15:03:55 +0200

    2.6.3 one warning less

--- usr/share/arcolinux-welcome-app/GUI.py
+++ usr/share/arcolinux-welcome-app/GUI.py
@@ -344,7 +344,7 @@ def GUI(self, Gtk, GdkPixbuf):
     # ======================================================================
     #                   PACK TO WINDOW
     # ======================================================================
-    label3 = Gtk.Label("v20.6-2")
+    label3 = Gtk.Label("v20.6-3")
     hbox7.pack_end(label3, False, False, 0)
     # if self.is_connected():
     #     self.get_message(label3, label4)
