15cf1c8c0401ab2ba55ac4e09afc165560fa82d7
Erik Dubois <erik.dubois@gmail.com>
2021-08-04 15:17:06 +0200

    sudo

--- usr/share/arcolinux-welcome-app/arcolinux-welcome-app.py
+++ usr/share/arcolinux-welcome-app/arcolinux-welcome-app.py
@@ -57,16 +57,19 @@ class Main(Gtk.Window):
         print("Clicked")
 
     def on_ai_clicked(self, widget):
-        subprocess.Popen(["pkexec", "cp", "/etc/calamares/settings-beginner.conf", "/etc/calamares/settings.conf"], shell=False)    
+        t = threading.Thread(target=self.run_app, args=(["sudo", "cp", "/etc/calamares/settings-beginner.conf", "/etc/calamares/settings.conf"],))
+        t.daemon = True
+        t.start()
         subprocess.Popen(["/usr/bin/calamares_polkit", "-d"], shell=False)
      
     def on_aica_clicked(self, widget):
-        subprocess.Popen(["pkexec", "cp", "/etc/calamares/settings-advanced.conf", "/etc/calamares/settings.conf"], shell=False)
+        t = threading.Thread(target=self.run_app, args=(["sudo", "cp", "/etc/calamares/settings-advanced.conf", "/etc/calamares/settings.conf"],))
+        t.daemon = True
+        t.start()
         subprocess.Popen(["/usr/bin/calamares_polkit", "-d"], shell=False)  
 
     def on_gp_clicked(self, widget):
-        t = threading.Thread(target=self.run_app,
-                             args=(["/usr/bin/gparted"],))
+        t = threading.Thread(target=self.run_app, args=(["/usr/bin/gparted"],))
         t.daemon = True
         t.start()
 
