5e0ce92354d1be48fc3622bc2fcca3accc1d1a2b
arcolinuxz <arcolinuxinfo@gmail.com>
2024-02-15 09:48:10 +0100

    nimdow

--- usr/share/arcolinux-welcome-app/ui/MessageDialog.py
+++ usr/share/arcolinux-welcome-app/ui/MessageDialog.py
@@ -185,9 +185,7 @@ class MessageDialogBootloader(Gtk.Dialog):
 
                 Thread(target=self.run_app, args=(app_cmd,), daemon=True).start()
                 if os.path.exists("/usr/share/xsessions/nimdow.desktop"):
-                    subprocess.Popen(
-                        ["sudo", "-E", "/usr/bin/calamares", "-d"], shell=False
-                    )
+                    subprocess.Popen([self.calamares_polkit], shell=False)
                 else:
                     subprocess.Popen([self.calamares_polkit, "-d"], shell=False)
             else:
@@ -238,12 +236,10 @@ class MessageDialogBootloader(Gtk.Dialog):
                     bootloader_file,
                     "/etc/calamares/modules/bootloader.conf",
                 ]
+                Thread(target=self.run_app, args=(app_cmd,), daemon=True).start()
                 if os.path.exists("/usr/share/xsessions/nimdow.desktop"):
-                    subprocess.Popen(
-                        ["sudo", "-E", "/usr/bin/calamares", "-d"], shell=False
-                    )
+                    subprocess.Popen([self.calamares_polkit], shell=False)
                 else:
-                    Thread(target=self.run_app, args=(app_cmd,), daemon=True).start()
                     subprocess.Popen([self.calamares_polkit, "-d"], shell=False)
 
             else:
